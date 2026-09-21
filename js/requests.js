8b926ab992b42539debbee6696b9fdb9d63cedcf
Krimx <jlhanna2005@gmail.com>
2026-09-21 09:43:11 -0400

    Added displayName parsing and username fallback

--- js/requests.js
+++ js/requests.js
@@ -2,5 +2,11 @@ export function getUsername(authToken) {
     return "hakeBowling";
 }
 export function getDisplayName(authToken) {
-    return "Don Hake";
+    const displayName = "Don Hake"; //will be a database grab later
+    if (displayName == null) return getUsername();
+    else return parseDisplayName(displayName);
+}
+
+export function parseDisplayName(displayName) {
+    return displayName.split(" ")[0];
 }
