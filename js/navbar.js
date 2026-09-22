4264808feefcaa4d036f050070d8899634268590
eSkyline-eng <epate1@ycp.edu>
2026-09-22 11:50:44 -0400

    Quick fix

--- js/navbar.js
+++ js/navbar.js
@@ -40,14 +40,14 @@ export function loadNavbar() {
 
     const notLoggedInPfpDropdownHTML = `
     <div class="dropdown_area">
-        <div class="dropdown_section id="log-in">Log In</div>
-        <div class="dropdown_section" id="sign-up">Sign Up</div>
+        <div class="dropdown_section" id="login-button">Log In</div>
+        <div class="dropdown_section" id="signup-button">Sign Up</div>
     </div>
     `
 
     const loggedInPfpDropdownHTML = `
     <div class="dropdown_area">
-        <div class="dropdown_section id="account-settings">Account Settings</div>
+        <div class="dropdown_section" id="account-settings">Account Settings</div>
         <div class="dropdown_section" id="log-out">Log Out</div>
     </div>
     `
